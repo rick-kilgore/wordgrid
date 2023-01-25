5c357dfd15129cbe7c7c4ea5221f2b99442e33c8
Rick Kilgore <rick.kilgore@gmail.com>
2023-01-25 00:48:52 -0800

    move main logic into functions so I can call them in my gcloud function handler

--- main.py
+++ main.py
@@ -46,6 +46,43 @@ def load_trie() -> Trie:
       pickle.dump(dictwords, picklefile)
 
   return dictwords
+  
+
+def load_board(board_num: int, file: str) -> Grid:
+  grid: Grid
+  width: int
+  height: int
+  bspec: List[Dict[int, str]]
+  width, height, bspec = get_board_data(board_num)
+
+  grid = grid_from_file(file, width, height, bspec)
+  # grid = test.test_grid_by_name(test_name)
+  print(grid.show())
+  return grid
+
+def run_for_single_pos(grid: Grid, trie: Trie, letters: str, startx: int, starty: int) -> Dict[str, FoundWord]:
+  print(f"searching from ({startx},{starty})", flush=True)
+  words: Dict[str, FoundWord] = {}
+  if startx < grid.w - 1:
+    # print("searching right...", flush=True)
+    srch = SearchCriteria(grid, grid.at(startx, starty), Dir.RIGHT, letters, trie)
+    words = findwords(srch)
+  if starty < grid.h - 1:
+    # print("searching down...", flush=True)
+    srch = SearchCriteria(grid, grid.at(startx, starty), Dir.DOWN, letters, trie)
+    addwords(words, findwords(srch))
+  return words
+
+
+def run_whole_board(grid: Grid, trie: Trie, file: str, letters: str) -> Dict[str, FoundWord]:
+  words: Dict[str, FoundWord] = {}
+  for y in range(grid.h):
+    for x in range (grid.w):
+      cell: Cell = grid.at(x, y)
+      if cell.value is None:
+        addwords(words, run_for_single_pos(grid, trie, letters, x, y))
+
+  return words
 
 
 # main
@@ -53,56 +90,22 @@ parser = argparse.ArgumentParser(description="Sync folders with Google Drive")
 parser.add_argument("-f", "--file", help="used named test data as input")
 parser.add_argument("-b", "--board", default=0, type=int, metavar="VERSION", help="use one of the solo match grids")
 parser.add_argument("-t", "--test", help="used named test data as input")
-parser.add_argument("-p", "--pos", nargs=2, metavar="N", help="look for words starting at x y pos")
+parser.add_argument("-p", "--pos", nargs=2, type=int, metavar="N", help="look for words starting at x y pos")
 parser.add_argument("letters", default="", help="letters from which to build words")
 args = parser.parse_args()
 
-grid: Grid
-width: int
-height: int
-bspec: List[Dict[int, str]]
-width, height, bspec = get_board_data(args.board)
-
-if args.file:
-  grid = grid_from_file(args.file, width, height, bspec)
-elif args.test:
-  grid = test.test_grid_by_name(args.test)
-
-print(grid.show())
-
-# letters = "thkwlfe"
+grid: Grid = load_board(args.board, args.file)
 trie: Trie = load_trie()
 words: Dict[str, FoundWord]
 if args.pos:
-  x = int(args.pos[0])
-  y = int(args.pos[1])
-  print(f"searching from ({x},{y}) w={width} h={height} letters={args.letters}", flush=True)
-  print("searching right...", flush=True)
-  srch: SearchCriteria = SearchCriteria(grid, grid.at(x, y), Dir.RIGHT, args.letters, trie)
-  words = findwords(srch)
-  print("searching down...", flush=True)
-  srch = SearchCriteria(grid, grid.at(x, y), Dir.DOWN, args.letters, trie)
-  addwords(words, findwords(srch))
-
+  words = run_for_single_pos(grid, trie, args.letters, args.pos[0], args.pos[1])
 else:
-  words = {}
-  for y in range(grid.h):
-    for x in range (grid.w):
-      cell: Cell = grid.at(x, y)
-      if cell.value is None:
-        print(f"start point: ({x},{y})", flush=True)
-        if x < grid.w - 1:
-          srch: SearchCriteria = SearchCriteria(grid, grid.at(x, y), Dir.RIGHT, args.letters, trie)
-          wlist = findwords(srch)
-          addwords(words, wlist)
-        if y < grid.h - 1:
-          srch: SearchCriteria = SearchCriteria(grid, grid.at(x, y), Dir.DOWN, args.letters, trie)
-          wlist = findwords(srch)
-          addwords(words, wlist)
+  words = run_whole_board(grid, trie, args.file, args.letters)
+
 
 srtd: List[str] = sorted(words.keys(), key=lambda w: words[w].score)
-out: str = "\n  ".join([str(words[w]) for w in srtd])
-print(f"found:\n{out}")
+full: str = "\n  ".join([str(words[w]) for w in srtd])
+print(f"found:\n{full}")
 
 print(f"\ntop 10 are:")
 for w in srtd[-10:]:
